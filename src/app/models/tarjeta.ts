c76812800ddb21771a3ebe2670ff572e6283fb5c
brisariffo9 <brisariffo@gmail.com>
2025-10-07 16:50:30 -0300

    repaso de mediaquery creacion de primera interfaz y uso de directiva ngfor para tarjetas

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,8 @@
+export interface Tarjeta {
+    id: string; // numeros como letras -->eje 
+    img: string;
+    alt: string;
+    titulo: string;
+    descripcion: string;
+    fechapublicacion: number;
+}
--- src/app/page/products/products.ts
+++ src/app/page/products/products.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tarjeta } from '../../models/tarjeta';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
+public infotarjeta: Tarjeta[];
+
+constructor(){
+  this.infotarjeta = [
+    { id:"1T",
+      img:"../../../../assets/diesel.jpg",
+      alt:"cartera diesel",
+      titulo:"cartera",
+      descripcion:"Foto de cartera",
+      fechapublicacion:0
+    },
+    {
+      id:"2T",
+      img:"../../../../assets/aros.jpg",
+      alt:"aros",
+      titulo:"aros dorados",
+      descripcion:"Foto de aros",
+      fechapublicacion:0
+    },
+    {
+      id:"3T",
+      img:"../../../../assets/lv.jpg",
+      alt:"cartera lv",
+      titulo:"cartera ",
+      descripcion:"Foto decartera LV",
+      fechapublicacion:0
+    },
+    {
+      id:"4T",
+      img:"../../../../assets/cartera rosa lv.jpg",
+      alt:"cartera lv rosa",
+      titulo:"cartera rosada ",
+      descripcion:"Foto decartera LV rosa",
+      fechapublicacion:0
+    }
+  ]
+}
 
 }
