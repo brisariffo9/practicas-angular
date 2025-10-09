f0f0e4476d0b862e46d3e978df264fdfc4fa0a87
brisariffo9 <brisariffo@gmail.com>
2025-10-09 16:51:46 -0300

    repaso de interfaces y directiva ngfor;aolicamos,las unidades de tamaño desde el html;aplicamos modal para ver informacion mas detallada de tarjetas

--- src/app/page/products/products.ts
+++ src/app/page/products/products.ts
@@ -16,9 +16,9 @@ constructor(){
     { id:"1T",
       img:"../../../../assets/diesel.jpg",
       alt:"cartera diesel",
-      titulo:"cartera",
+      titulo:"cartera Diesel",
       descripcion:"Foto de cartera",
-      fechapublicacion:0
+      fechapublicacion:25
     },
     {
       id:"2T",
@@ -26,25 +26,30 @@ constructor(){
       alt:"aros",
       titulo:"aros dorados",
       descripcion:"Foto de aros",
-      fechapublicacion:0
+      fechapublicacion:25
     },
     {
       id:"3T",
       img:"../../../../assets/lv.jpg",
       alt:"cartera lv",
-      titulo:"cartera ",
+      titulo:"cartera LV ",
       descripcion:"Foto decartera LV",
-      fechapublicacion:0
+      fechapublicacion:25
     },
     {
       id:"4T",
       img:"../../../../assets/cartera rosa lv.jpg",
       alt:"cartera lv rosa",
-      titulo:"cartera rosada ",
+      titulo:"cartera LV rosada ",
       descripcion:"Foto decartera LV rosa",
-      fechapublicacion:0
-    }
+      fechapublicacion:25
+    },
   ]
+}
+tarjetaseleccionada:any = null
+
+verMas(tarjeta:any){
+  this.tarjetaseleccionada = tarjeta;
 }
 
 }
